1b560f9a66c7e58df8a3f4b4c3a9626cd5be912a
brandon <dmdjqn@gmail.com>
2022-06-08 18:02:12 +0900

    feat: 컨트롤러 생성

--- src/main/java/com/bradon/jwtpractice/controller/DemoController.java
+++ src/main/java/com/bradon/jwtpractice/controller/DemoController.java
@@ -1,4 +1,14 @@
 package com.bradon.jwtpractice.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping("/api/hello")
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("hello");
+    }
 }
